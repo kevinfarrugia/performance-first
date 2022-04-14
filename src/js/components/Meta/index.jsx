e724faf3773ff7724fad0fcb0eee24ae13dff6fa
Kevin Farrugia <hello@imkev.dev>
2022-04-15 00:17:11 +0200

    Updated icon & meta

--- src/js/components/Meta/index.jsx
+++ src/js/components/Meta/index.jsx
@@ -8,16 +8,6 @@ function Meta({ title, description, keywords }) {
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
       <meta property="og:title" content={title} />
-      <meta charset="utf-8" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="theme-color" content="#fff" />
-      <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
-      <link rel="icon" sizes="192x192" href="/icon.png" />
-      <link rel="apple-touch-icon" href="/icon.png" />
-      <link rel="manifest" href="/site.webmanifest" />
-      <meta property="og:image" content="/img/logo.jpg" />
-      <link href="/img/logo.jpg" />
     </Helmet>
   );
 }
